8804f1f635be6583a6e2ab196c46c896d42f460d
Chris Tack <chris_tack@mac.com>
2019-12-27 16:54:45 -0800

    Create recipe schema

---
+++ models/Recipe.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = recipe.Schema;
+
+var recipeSchema = new Schema({
+    recipeID: Number,
+    recipeName: String,
+    ingredient: [{type: Schema.Types.ObjectId, ref: 'Ingredient'}],
+    directions: String,
+    preparation_time: Number,
+    cooking_time: Number,
+    serves: Number,
+    ratings: [{type: Schema.Types.ObjectId, ref: 'Rating'}]
+})
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+module.exports = Recipe;
