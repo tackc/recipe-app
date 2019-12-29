49b716fecf41033f82332c140f4952bb7a74a958
Chris Tack <chris_tack@mac.com>
2019-12-28 22:01:16 -0800

    Hide sign in with Apple button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Signin from './Authentication/Signin';
+// import Signin from './Authentication/Signin';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
-      <Signin />
+      {/* <Signin /> */}
     </div>
   );
 }
