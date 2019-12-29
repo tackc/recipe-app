1d7dd188601f422c197b1064c24ae9383740accd
Chris Tack <chris_tack@mac.com>
2019-12-28 22:15:17 -0800

    set up connection to MongoDB

---
+++ config/database.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI);
+
+var db = mongoose.connection;
+
+db.once('open', () => {
+    console.log(`Connected to MongoDB ${db.name} at ${db.host}: $${db.port}`);
+})
