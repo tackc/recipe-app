b6510e8959f60522ee8410c5c8aac9c9fad8030e
Chris Tack <chris_tack@mac.com>
2019-12-28 20:16:50 -0800

    Fix typo

--- src/Authentication/Signin.js
+++ src/Authentication/Signin.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './Signin.css';
 
-class Signup extends Component {
+class Signin extends Component {
     constructor(props) {
         super(props)
         this.state = {
