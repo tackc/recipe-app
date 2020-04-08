a3b7231c0a1e9cc1c2af8a64c62bc0283b02ff72
Chris Tack <chris_tack@mac.com>
2020-04-07 21:46:33 -0700

    Convert to class based component and add state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Signin from './Authentication/Signin';
 
-function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header"></header> */}
-      <Signin />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      token: '',
+      user: null,
+      error: null,
+      lockedResult: '',
+      categories: ["Main Dishes", "Appetizers", "Side Dishes", "Desserts", "Drinks"]
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* <header className="App-header"></header> */}
+        <Signin />
+      </div>
+    );
+  }
 }
 
 export default App;
