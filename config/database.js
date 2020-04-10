ea000ffcbaf9dd5ca2704cc9f73822a58f83f008
Chris Tack <chris_tack@mac.com>
2020-04-09 21:34:42 -0700

    Fix typo

--- config/database.js
+++ config/database.js
@@ -5,5 +5,5 @@ mongoose.connect(process.env.MONGODB_URI);
 var db = mongoose.connection;
 
 db.once('open', () => {
-    console.log(`Connected to MongoDB ${db.name} at ${db.host}: $${db.port}`);
+    console.log(`Connected to MongoDB ${db.name} at ${db.host}: ${db.port}`);
 })
