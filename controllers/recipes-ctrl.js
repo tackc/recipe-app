f72acdb60c8fafa9c54a8814cf3280db746d3c2d
Chris Tack <chris_tack@mac.com>
2020-04-22 09:20:08 -0700

    Create function to create a recipe

--- controllers/recipes-ctrl.js
+++ controllers/recipes-ctrl.js
@@ -1,10 +1,37 @@
 var Recipe = require('../models/Recipe');
 
-module.exports = {
-    create
-}
+function createRecipe(req, res) {
+    const body = req.body
 
-function create(req, res) {
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must enter all required recipe fields',
+        })
+    }
+
+    const recipe = new Recipe(body);
+
+    if (!recipe) {
+        return res.status(400).json({ success: false, error: err });
+    }
+
+    recipe
+        .save()
+        .then(() => {
+            return res.status(201).json({
+                success: true,
+                id: recipe._id,
+                message: 'Recipe created!',
+            })
+        })
+        .catch(error => {
+            return res.status(400).json({
+                error,
+                message: 'There was an error. The recipe was not created',
+            })
+        })
+
     Recipe.create(req.body)
     .then(recipe => {
         res.json(recipe);
@@ -13,3 +40,7 @@ function create(req, res) {
         res.json({error: err})
     })
 }
+
+module.exports = {
+    createRecipe,
+};
