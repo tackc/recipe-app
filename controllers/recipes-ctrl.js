31befb9dad08f4bc557978edc2b9954e9c00999a
Chris Tack <chris_tack@mac.com>
2020-04-22 09:53:59 -0700

    Create function to update a recipe

--- controllers/recipes-ctrl.js
+++ controllers/recipes-ctrl.js
@@ -13,7 +13,7 @@ function createRecipe(req, res) {
     const recipe = new Recipe(body);
 
     if (!recipe) {
-        return res.status(400).json({ success: false, error: err });
+        return res.status(400).json({ success: false, error: 'There is no recipe being sent from the form to the server' });
     }
 
     recipe
@@ -27,20 +27,63 @@ function createRecipe(req, res) {
         })
         .catch(error => {
             return res.status(400).json({
-                error,
-                message: 'There was an error. The recipe was not created',
+                message: `The recipe was not created due to the following error: ${error}`,
             })
         })
+}
 
-    Recipe.create(req.body)
-    .then(recipe => {
-        res.json(recipe);
-    })
-    .catch(err => {
-        res.json({error: err})
+async function updateRecipe(req, res) {
+    const body = req.body;
+
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'Recipe not found',
+        })
+    }
+
+    Recipe.findOne({ _id: req.params.id }, (err, recipe) => {
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Recipe not found',
+            })
+        }
+
+        recipe.name = body.name
+        recipe.description = body.description
+        recipe.ingredientQuantity = body.ingredientQuantity
+        recipe.ingredient = body.ingredient
+        recipe.instructions = body.instructions
+        recipe.preparation_time = body.preparation_time
+        recipe.cooking_time = body.cooking_time
+        recipe.total_time = body.total_time
+        recipe.serves = body.serves
+        recipe.notes = body.notes
+        recipe.author = body.author
+        recipe.url = body.url
+        recipe.ratings = body.ratings
+        recipe.images = body.images
+
+        recipe
+            .save()
+            .then(() => {
+                return res.status(200).json({
+                    success: true,
+                    id: recipe._id,
+                    message: 'Recipe updated!'
+                })
+            })
+            .catch(error => {
+                return res.status(404).json({
+                    success: true,
+                    message: `Recipe not updated due to the following error: ${error}`
+                })
+            })
     })
 }
 
 module.exports = {
     createRecipe,
+    updateRecipe,
 };
