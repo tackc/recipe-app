020d81ae4d334287c051b88b2e1c01bd13bcaeae
Chris Tack <chris_tack@mac.com>
2020-04-28 09:42:22 -0700

    Add error handling to db and export db

--- config/database.js
+++ config/database.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose
+    .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .catch(e => {
+        console.error('Connection error', e.message)
+    });
 
 var db = mongoose.connection;
 
 db.once('open', () => {
     console.log(`Connected to MongoDB ${db.name} at ${db.host}: ${db.port}`);
 })
+
+module.exports = db;
