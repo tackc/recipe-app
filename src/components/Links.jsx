36177063ad55cb0356226bc8e8314a2ce816ead1
Chris Tack <chris_tack@mac.com>
2020-04-28 14:17:28 -0700

    Create Links component

---
+++ src/components/Links.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Collapse = styled.div.attrs({
+    className: 'collapse navbar-collapse',
+})``
+
+const List = styled.div.attrs({
+    className: 'navbar-nav mr-auto',
+})``
+
+const Item = styled.div.attrs({
+    className: 'collapse navbar-collapse',
+})``
+
+class Links extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Link to='/' className='navbar-brand'>
+                    Recipe App
+                </Link>
+                <Collapse>
+                    <List>
+                        <Item>
+                            <Link to='/' className='nav-link'>
+                                Recipes
+                            </Link>
+                        </Item>
+                        <Item>
+                            <Link to='/create' className='nav-link'>
+                                Add Recipe
+                            </Link>
+                        </Item>
+                    </List>
+                </Collapse>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Links;
