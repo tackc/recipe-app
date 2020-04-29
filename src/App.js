0ad054ec6159bebd0ce45a0bb18d2166c802394d
Chris Tack <chris_tack@mac.com>
2020-04-29 09:12:21 -0700

    Create update route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 // import Signin from './Authentication/Signin';
 // import Categories from './pages/Categories';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { RecipesList, RecipesInsert, RecipeCard } from './pages';
+import { RecipesList, RecipesInsert, RecipesUpdate, RecipeCard } from './pages';
 import { NavBar } from './components';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -34,8 +34,9 @@ class App extends Component {
           <Switch>
             <Route path='/login' render={ () => <p>This is the route for creating an account!</p>} />
             <Route path='/signup' render={ () => <p>This is the signup route!</p>} />
-            <Route path='/recipes/list' render={ () => <RecipesList /> } />
+            <Route path='/recipes/list' exact component={RecipesList} />
             <Route path='/recipes/create' exact component={RecipesInsert} />
+            <Route path='/recipes/update/:id' exact component={RecipesUpdate} />
             <Route exact path='/' render={ (props) => `<p>Welcome ${this.state.user}</p>` } />
           </Switch>
         </Router>
