ba4a91deee632ee3d780e718a3eb55c739b567af
Chris Tack <chris_tack@mac.com>
2020-04-29 09:16:07 -0700

    Add import and export for RecipesUpdate

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import RecipesList from './RecipesList';
 import RecipesInsert from './RecipesInsert';
+import RecipesUpdate from './RecipesUpdate';
 import RecipeCard from './RecipeCard';
 
-export { RecipesList, RecipesInsert, RecipeCard };
+export { RecipesList, RecipesInsert, RecipesUpdate, RecipeCard };
