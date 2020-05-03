d05b832981f4570c1e9255d0063eec78a82c3364
Chris Tack <chris_tack@mac.com>
2020-05-03 14:47:07 -0700

    Refactor component to build hamburger menu + Bootstrap

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,24 +4,51 @@ import styled from 'styled-components';
 import Logo from './Logo';
 import Links from './Links';
 
-const Container = styled.div.attrs({
-    className: 'nav-container'
+const Container = styled.nav.attrs({
+    className: 'navbar navbar-expand-lg navbar-dark bg-dark'
 })``
 
-const Nav = styled.nav.attrs({
-    className: 'navbar navbar-expand-lg navbar-dark bg-dark min-vw-100',
-})`
-    margin-bottom: 20px
-`
+const Hamburger = styled.button.attrs({
+    className: 'navbar-toggler',
+    type: 'button'
+})``
+
+const HamburgerToggle = styled.span.attrs({
+    className: 'navbar-toggler-icon'
+})``
+
+const NavContainer = styled.div.attrs({
+})``
 
 class NavBar extends Component {
+constructor(props) {
+    super(props);
+    this.state = {
+        menu: false
+    };
+    this.toggleMenu = this.toggleMenu.bind(this);
+}
+
+toggleMenu() {
+    this.setState({ menu: !this.state.menu })
+}
+
     render() {
+    const show = (this.state.menu) ? 'show' : '';
+
         return (
             <Container>
-                <Nav>
-                    <Logo />
+                
+                <Logo />
+
+                <Hamburger onClick={ this.toggleMenu }>
+                    <HamburgerToggle />
+                </Hamburger>
+                
+                <NavContainer className={'collapse navbar-collapse ' + show }>
                     <Links />
-                </Nav>
+                </NavContainer>
+
             </Container>
         )
     }
