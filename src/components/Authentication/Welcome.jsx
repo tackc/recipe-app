78765ae8b30af9d275bf6f93183f3c35bc4c8425
Chris Tack <chris_tack@mac.com>
2020-05-21 14:42:14 -0700

    Create Welcome component

---
+++ src/components/Authentication/Welcome.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Title = styled.h4.attrs({
+    className: 'text-center'
+})``
+
+const Text = styled.p``
+
+const Container = styled.div.attrs({
+    className: 'container my-5 text-center'
+})``
+
+const Button = styled.button.attrs({
+    className: 'btn mx-3'
+})``
+
+const Welcome = (props) => {
+    let content;
+    
+    if (props.user) {
+    content = <Title>Welcome { props.user.firstName }</Title>
+    } else {
+        content = (
+            <Container>
+                <Link to='/signup'>
+                    <Button className='btn-warning'>SIGN UP</Button>
+                </Link>
+                <Link to='/login'>
+                    <Button className='btn-success'>LOG IN</Button>
+                </Link>
+            </Container>
+        )
+    }
+
+    return (
+        <Container>
+            <Title>Welcome to the Recipe App</Title>
+            <Text>You are probably thinking 'there are hundreds of recipe apps / websites out there...why do I need another one'. Well, I really enjoy cooking and baking & needed a place to store all my recipes in one place without dealing without all the ads or worrying about them being deleted.</Text>
+            {content}
+        </Container>
+    )
+}
+
+export default Welcome;
