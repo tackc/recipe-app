7bde73c76af1453cb7a86f4492c9a7ab7b8ad544
Chris Tack <chris_tack@mac.com>
2020-05-21 11:37:14 -0700

    Export auth components

---
+++ src/components/Authentication/index.js
@@ -0,0 +1,6 @@
+import Login from './Login';
+import Signup from './Signup';
+import Welcome from './Welcome';
+import ErrorPanel from './ErrorPanel';
+
+export { Login, Signup, Welcome, ErrorPanel };
