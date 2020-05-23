9d542519400421cff1ce7d170d3b906b6ff1d70b
Chris Tack <chris_tack@mac.com>
2020-05-23 13:55:00 -0700

    Install dependency

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var path = require( 'path' );
 var favicon = require( 'serve-favicon' );
 var logger = require( 'morgan' );
 const expressJWT = require('express-jwt')
+const RateLimit = require('express-rate-limit');
 
 const apiRoutes = require('./routes/recipe-router');
 const auth = require('./routes/auth');
