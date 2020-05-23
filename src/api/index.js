8e20e2e22c6a3d4cfb29a67b697b955401b1310e
Chris Tack <chris_tack@mac.com>
2020-05-23 14:00:50 -0700

    Add comment

--- src/api/index.js
+++ src/api/index.js
@@ -4,12 +4,14 @@ const api = axios.create({
     baseURL: 'http://localhost:3000/api'
 });
 
+// Recipe Routes
 export const insertRecipe = payload => api.post(`/recipe`, payload);
 export const getAllRecipes = () => api.get(`/recipes`);
 export const updateRecipeById = (id, payload) => api.put(`/recipe/${id}`, payload);
 export const deleteRecipeById = id => api.delete(`/recipe/${id}`);
 export const getRecipeById = id => api.get(`/recipe/${id}`);
 
+// Ingredient Routes
 export const insertIngredient = payload => api.post(`/ingredient`, payload);
 export const getAllIngredients = () => api.get(`/ingredients`);
 export const getIngredientById = id => api.get(`/ingredient/${id}`);
