5ac4d928834a2db25c9e62e6a645890d5f8e1290
Chris Tack <chris_tack@mac.com>
2020-05-23 21:13:13 -0700

    Comment out unused code

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
