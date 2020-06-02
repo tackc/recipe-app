eee95d6c08981920ce25c848507d80bd3996cd9f
Chris Tack <chris_tack@mac.com>
2020-06-02 09:25:10 -0700

    Create Measurements component

---
+++ src/components/InsertRecipe/Measurements.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Row = styled.div.attrs({
+    className: 'form-group m-4'
+})``
+
+const Label = styled.label.attrs({
+    className: 'font-weight-bold text-left mb-0'
+})`
+    display: block;
+    padding-left: 2px;
+    padding-bottom: 2px;
+    font-size: .8em;
+`
+
+const InputText = styled.input.attrs({
+    className: 'col'
+})``
+
+const DropdownWrapper = styled.div.attrs({
+    className: 'col'
+})`margin: auto`
+
+const DropdownSelect = styled.select.attrs({
+    className: 'btn'
+})``
+
+const DropdownItem = styled.option``
+
+export default function MeasurementsDropdown() {
+    return(
+        <Row>
+            <Label>Ingredient Quantity: </Label>
+            <InputText
+                type="text"
+                value={ props.ingredient_quantity }s
+                name='ingredient_quantity'
+                onChange={handleChange}
+            />
+
+            <Label>Unit: </Label>
+            <DropdownWrapper>
+                <DropdownSelect name='unit' onChange={handleChange} value={props.unit}>
+                    <DropdownItem value='' defaultValue='selected'>Unit of Measurement</DropdownItem>
+                    <DropdownItem value='pinch'>Pinch</DropdownItem>
+                    <DropdownItem value='Teaspoon'>Teaspoon</DropdownItem>
+                    <DropdownItem value='Tablespoon'>Tablespoon</DropdownItem>
+                    <DropdownItem value='Fluid ounce'>Fluid ounce</DropdownItem>
+                    <DropdownItem value='Cup'>Cup</DropdownItem>
+                    <DropdownItem value='Pint'>Pint</DropdownItem>
+                    <DropdownItem value='Quart'>Quart</DropdownItem>
+                    <DropdownItem value='Gallon'>Gallon</DropdownItem>
+                </DropdownSelect>
+            </DropdownWrapper>
+
+            <Label>Ingredients: </Label>
+            <InputText
+                type="text"
+                value={ props.ingredients }
+                name='ingredients'
+                onChange={handleChange}
+            />
+        </Row>
+    )
+}
