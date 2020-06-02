e2193d1755afe052cdb7621dc8d7a4643c5addaf
Chris Tack <chris_tack@mac.com>
2020-06-02 15:08:02 -0700

    Refactor component

--- src/components/InsertRecipe/InputFieldForm.jsx
+++ src/components/InsertRecipe/InputFieldForm.jsx
@@ -7,120 +7,127 @@ const Wrapper = styled.div.attrs({
     className: 'container border-top border-bottom p-4 my-5'
 })``
 
-const Title = styled.h1``
-
-const Label = styled.label``
-
-const Input = styled.input.attrs({
-    className: 'col'
+const Title = styled.h5.attrs({
+    className: 'text-center'
 })``
 
-const Ingredient = styled.div``
+const Row = styled.div.attrs({
+    className: 'form-group m-4'
+})``
 
-const Hr = styled.hr``
+const Label = styled.label.attrs({
+    className: 'font-weight-bold text-left mb-0'
+})`
+    display: block;
+    padding-left: 2px;
+    padding-bottom: 2px;
+    font-size: .8em;
+`
+
+const InputText = styled.input.attrs({
+    className: 'col',
+    type: 'text'
+})``
 
 const Button = styled.button.attrs({
+    type: 'button',
     className: 'btn'
 })``
 
 const InputFieldForm = (props) => {
     const [inputFields, setInputFields] = useState([
-        { firstName: '', lastName: '' }
+        { ingredientQuantity: '', ingredientUnit: '', ingredient: '' }
     ]);
 
     const handleAddFields = () => {
         const values = [...inputFields];
-        values.push({ firstName: '', lastName: '' });
+        values.push({ ingredientQuantity: '', ingredientUnit: '', ingredient: '' });
         setInputFields(values);
     };
 
     const handleRemoveFields = index => {
-    const values = [...inputFields];
-    values.splice(index, 1);
-    setInputFields(values);
+        const values = [...inputFields];
+        values.splice(index, 1);
+        setInputFields(values);
     };
 
     const handleInputChange = (index, event) => {
-    const values = [...inputFields];
-    if (event.target.name === "firstName") {
-        values[index].firstName = event.target.value;
-    } else {
-        values[index].lastName = event.target.value;
-    }
+        const values = [...inputFields];
+        if (event.target.name === "ingredient") {
+            values[index].ingredient = event.target.value;
+        } else {
+            values[index].lastName = event.target.value;
+        }
 
-    setInputFields(values);
+        setInputFields(values);
     };
 
     const handleSubmit = e => {
-    e.preventDefault();
-    console.log("inputFields", inputFields);
+        e.preventDefault();
+        console.log("inputFields", inputFields);
     };
 
     return (
-    <>
-            <h1>Dynamic Form Fields in React</h1>
+        <Wrapper>
+            <Title>Ingredients</Title>
             <form onSubmit={handleSubmit}>
-                <div className="form-row">
-                    <Measurements unit_of_measurement={props.unit_of_measurement} handleChange={props.handleChange} />
                 {inputFields.map((inputField, index) => (
                     <Fragment key={`${inputField}~${index}`}>
-                    <div className="form-group col-sm-6">
-                        <label htmlFor="firstName">First Name</label>
-                        <input
-                        type="text"
-                        className="form-control"
-                        id="firstName"
-                        name="firstName"
-                        value={inputField.firstName}
-                        onChange={event => handleInputChange(index, event)}
-                        />
-                    </div>
-                    <div className="form-group col-sm-4">
-                        <label htmlFor="lastName">Last Name</label>
-                        <input
-                        type="text" 
-                        className="form-control" 
-                        id="lastName"
-                        name="lastName"
-                        value={inputField.lastName}
-                        onChange={event => handleInputChange(index, event)}
-                        />
-                    </div>
-                    <div className="form-group col-sm-2">
-                        <button
-                        className="btn btn-link"
-                        type="button"
-                        onClick={() => handleRemoveFields(index)}
-                        >
-                        -
-                        </button>
-                        <button
-                        className="btn btn-link"
-                        type="button"
-                        onClick={() => handleAddFields()}
-                        >
-                        +
-                        </button>
-                    </div>
+                        <Row>
+                            <Measurements unit_of_measurement={props.unit_of_measurement} handleChange={props.handleChange} />
+                            
+                            <Label htmlFor="ingredient">Ingredient</Label>
+                            <InputText
+                                id="ingredient"
+                                name="ingredient"
+                                value={inputField.ingredient}
+                                onChange={event => handleInputChange(index, event)}
+                            />
+
+                            {/* <Label htmlFor="lastName">Last Name</Label>
+                            <InputText
+                                id="lastName"
+                                name="lastName"
+                                value={inputField.lastName}
+                                onChange={event => handleInputChange(index, event)}
+                            /> */}
+
+                            <Button
+                                className="btn btn-link"
+
+                                onClick={() => handleRemoveFields(index)}
+                                >
+                                -
+                            </Button>
+                            <Button
+                                className="btn btn-link"
+
+                                onClick={() => handleAddFields()}
+                                >
+                                +
+                            </Button>
+                        </Row>
+
                     </Fragment>
                 ))}
-                </div>
-                <div className="submit-button">
-                <button
-                    className="btn btn-primary mr-2"
-                    type="submit"
-                    onSubmit={handleSubmit}
-                >
-                    Save
-                </button>
-                </div>
+
+                {/* <div className="submit-button">
+                    <button
+                        className="btn btn-primary mr-2"
+                        type="submit"
+                        onSubmit={handleSubmit}
+                    >
+                        Save
+                    </button>
+                </div> */}
+
                 <br/>
                 <pre>
-                {JSON.stringify(inputFields, null, 2)}
+                    {JSON.stringify(inputFields, null, 2)}
                 </pre>
             </form>
-            </>
-        );
+        </Wrapper>
+    );
 };
 
 export default InputFieldForm;
