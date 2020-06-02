854fe44171a4d0be38f61e8a0de9190aca675bb1
Chris Tack <chris_tack@mac.com>
2020-06-02 09:40:19 -0700

    File reorganization

--- src/components/NavBar/Logo.jsx
+++ src/components/NavBar/Logo.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-import logo from '../logo.svg';
+import logo from '../../logo.svg';
 
 const Wrapper = styled.a.attrs({
     className: 'navbar-brand',
