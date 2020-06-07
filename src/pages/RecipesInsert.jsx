1cc54b3ce886162dd95140d633734f3db305b0b8
Chris Tack <chris_tack@mac.com>
2020-06-07 14:57:31 -0700

    Import Rating

--- src/pages/RecipesInsert.jsx
+++ src/pages/RecipesInsert.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import api from '../api';
+import Rating from '@bit/nexxtway.react-rainbow.rating';
 // import IngredientInsert from './IngredientInsert';
 
 import styled from 'styled-components';
