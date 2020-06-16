d8405d895ec5093311b25f73d503bfdf5082fdf0
Chris Tack <chris_tack@mac.com>
2020-06-15 21:33:59 -0700

    Change from put to patch

--- routes/recipe-router.js
+++ routes/recipe-router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const RecipeCtrl = require('../controllers/recipes-ctrl');
 
 router.post('/recipes', RecipeCtrl.insertRecipe);
-router.put('/recipes/:id', RecipeCtrl.updateRecipe);
+router.patch('/recipes/:id', RecipeCtrl.updateRecipe);
 router.delete('/recipes/:id', RecipeCtrl.deleteRecipe);
 router.get('/recipes/:id', RecipeCtrl.getRecipeById);
 router.get('/recipes', RecipeCtrl.getRecipes);
