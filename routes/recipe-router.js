dbecfab60f416546850738b890244a863341b38f
Chris Tack <chris_tack@mac.com>
2020-06-17 21:47:09 -0700

    Update function name

--- routes/recipe-router.js
+++ routes/recipe-router.js
@@ -4,7 +4,7 @@ const RecipeCtrl = require('../controllers/recipes-ctrl');
 
 router.post('/recipes', RecipeCtrl.insertRecipe);
 router.patch('/recipes/:id', RecipeCtrl.updateRecipe);
-router.delete('/recipes/:id', RecipeCtrl.deleteRecipe);
+router.delete('/recipes/:id', RecipeCtrl.deleteRecipeById);
 router.get('/recipes/:id', RecipeCtrl.getRecipeById);
 router.get('/recipes', RecipeCtrl.getRecipes);
 
