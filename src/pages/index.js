aeb6e7ce3e2c339268c2556c0de238dfbe594773
Chris Tack <chris_tack@mac.com>
2020-06-28 21:30:42 -0700

    Import and export RecipeView

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,6 @@ import RecipesInsert from './RecipesInsert';
 import RecipesUpdate from './RecipesUpdate';
 import RecipeCard from './RecipeCard';
 import BasicRecipeList from './BasicRecipesList';
+import RecipeView from './RecipeView';
 
-export { RecipesList, RecipesInsert, RecipesUpdate, RecipeCard, BasicRecipeList };
+export { RecipesList, RecipesInsert, RecipesUpdate, RecipeCard, BasicRecipeList, RecipeView };
