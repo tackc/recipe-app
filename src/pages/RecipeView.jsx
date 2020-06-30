f59bad343d621a9e61ef49c0c00f22c5240f186f
Chris Tack <chris_tack@mac.com>
2020-06-29 19:30:24 -0700

    Use axios

--- src/pages/RecipeView.jsx
+++ src/pages/RecipeView.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import apis from '../api';
+import api from '../api';
+import axios from 'axios';
 
 const Wrapper = styled.div.attrs({
     className: 'container'
@@ -15,23 +16,26 @@ const Title = styled.h1.attrs({
     color: #222;
 `
 
-const RecipeView = (props) => {
+const RecipeView = (...props) => {
     const [recipe, setRecipe] = useState([])
 
     useEffect(() => {
         async function getRecipeById() {
             try {
-                const response = await apis.getRecipeById();
-                setRecipe(response.data.data)
+                // const response = await api.getRecipeById();
+                const response = await axios.get(`/api/recipes/${props.match.params._id}`);
+                setRecipe(response.data)
+                console.log(response.data.data)
             } catch(error) {
                 console.log('error', error);
             }
         }
         getRecipeById();
-    }, []);
+    }, [props]);
 
     return(
         <Wrapper>
+            <Title>{recipe.name}</Title>
             <Title>{recipe.title}</Title>
         </Wrapper>
     )
